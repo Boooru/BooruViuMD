6b9c9e4c1c44b5a4fc094ae25a0278fcd87490d5
BooruViu <booruviu@gmail.com>
2021-11-03 18:44:30 -0500

    Updated nav drawer

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,15 +1,18 @@
 from kivy import Logger
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import AsyncImage, Image
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivymd.material_resources import dp
+from kivymd.theming import ThemableBehavior
 from kivymd.uix import MDAdaptiveWidget
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
-from kivymd.uix.list import MDList
+from kivymd.uix.list import MDList, OneLineIconListItem
 from kivymd.uix.selectioncontrol import MDSwitch
 
 import main
@@ -189,3 +192,37 @@ class SwitchArray(MDList):
                         continue
 
                     sub_child.active = False
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+
+
+class ContentNavigationDrawer(BoxLayout):
+
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+
+    pass
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
